88a74d5505f3e15b40ca800b5f441b0227ac4cb7
aayushghimire321 <aghimire491@gmail.com>
2024-11-24 09:20:18 +0545

    classwork

---
+++ bin/SingletonClass.dart
@@ -0,0 +1,23 @@
+
+class SingletonClass {
+static final SingletonClass _instance = SingletonClass._internal();
+// Private constructor
+
+// Private static variable
+SingletonClass._internal();
+// factory method which return only one instance
+factory SingletonClass() {
+return _instance;
+
+}
+
+}
+
+void main() {
+    // Create instance of SingletonClass
+    SingletonClass singletonClass = SingletonClass();
+    // Create another instance of SingletonClass 
+    SingletonClass singletonClass2 = SingletonClass();
+    // Check if both instances are same
+    print(singletonClass == singletonClass2); // true
+ }
