7cfe6135c411d16da0e60a5d82522475139be76b
aayushghimire321 <aghimire491@gmail.com>
2024-11-24 09:46:13 +0545

    message

---
+++ bin/person_object.dart
@@ -0,0 +1,22 @@
+void main(){
+    final person1 = Person(firstName: "Kiran", lastName: "Rana", age:22);
+    final person2 = Person(firstName: "Salman", lastName: "Khan", age:22);
+    final person3 = Person(firstName: "Katrina", lastName: "Kaif", age:22);
+    final person4 = Person(firstName: "Vicky", lastName: "Kaushal", age:22);
+    final person5 = Person(firstName: "Dahayang", lastName: "Rai", age:22);
+
+    //add person objectys to list
+    List<Person> lstPErson = [];
+    lstPerson.add(Person1);
+    lstPerson.add(Person2);
+    lstPerson.add(Person3);
+    lstPerson.add(Person4);
+    lstPerson.add(Person5);
+    //display person's name and age
+    for (Person p in lstPerson){
+      print("Name : ${p.firstName} and Ages: ${p.age}")
+    }
+}
+lstPerson.map(
+  (person) => print('Name : ${person.firstName} and Age: ${person.age}')
+);
